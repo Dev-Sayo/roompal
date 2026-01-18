33244b5213aab2084e57bd73ae6b8c0de0fb5661
Sayo <shittubusayo585@gmail.com>
2026-01-18 02:08:38 +0100

    working on the message app and listing form

---
+++ pages/reg-users/list-a-house/list-a-house.js
@@ -0,0 +1,4 @@
+"use strict";
+const selectField = document.getElementById("selectField");
+const selectText = document.querySelectorAll("#select");
+const selectField = document.getElementById("selectField");
